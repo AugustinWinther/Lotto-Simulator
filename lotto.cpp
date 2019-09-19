df2261a50c8ce4598be3850dbb655e231226c577
AugustinWinther <augustin@winther.io>
2019-09-19 18:09:58 +0200

    Changed variable and function names
    Changed the following variable and function names:
    - GenLottoArrays > GenLottoNumbers
    - PrintVectorArrays > PrintLottoNumbers
    - numberOfArrays > amountOfNumbers
    - userArrays > userNumbers
    - winningArray > winningNumbers

--- lotto.cpp
+++ lotto.cpp
@@ -11,9 +11,9 @@
 //N/A
 
 //Global declearations of functions
-std::vector<std::array<int, 7>> GenLottoArrays (int lottoType, int numberOfArrays, std::vector<std::array<int, 7>>& vector); //Generates arrays representing lotto numbers/rows
+std::vector<std::array<int, 7>> GenLottoNumbers (int lottoType, int amountOfNumbers, std::vector<std::array<int, 7>>& vector); //Generates arrays representing lotto numbers/rows
 void CheckIfWin (int lottoType, std::vector<std::array<int, 7>> user, std::vector<std::array<int, 7>> win); //Checks if user passed vector contains the first array from win vector, if not, it generates new win array (lotto numbers)
-void PrintVectorArrays(std::vector<std::array<int, 7>> vector); //Prints all arrays in a vector vertically
+void PrintLottoNumbers(std::vector<std::array<int, 7>> vector); //Prints all arrays in a vector vertically
 std::string NumToString(unsigned long num); //Converts number to string with spaces added after every 3rd digit for easy reading
 void menu();
 
@@ -61,14 +61,14 @@ void menu(){
             break;
         }
     }
-    int numberOfArrays; //Amopunt of different lotto numbers
-    while (numberOfArrays < 1 || numberOfArrays > 10000){
+    int amountOfNumbers; //Amopunt of different lotto numbers
+    while (amountOfNumbers < 1 || amountOfNumbers > 10000){
         std::cout << "Please type amount of lotto numbers you want [0 - 10 000]: ";
-        std::cin >> numberOfArrays;
-        if (numberOfArrays < 1 || numberOfArrays > 10000){ //Warns the user if they typed a inavlid number
+        std::cin >> amountOfNumbers;
+        if (amountOfNumbers < 1 || amountOfNumbers > 10000){ //Warns the user if they typed a inavlid number
             std::cout << "ERROR! Invalid input, please try again" << std::endl;
             std::cin.clear(); //Clears error flags in cin
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); //Ignores rest of the line inputet to numberOfArrays
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); //Ignores rest of the line inputet to amountOfNumbers
         } else {
             std::cout << std::endl; //New line for formatting sakes
             break;
@@ -76,24 +76,24 @@ void menu(){
     }
 
     std::cout << "Generating lotto numbers..." << std::endl;
-    std::vector<std::array<int, 7>> userArrays;     //Vector containing a dymanic amount of 7 int element arrays
-    GenLottoArrays(lottoType, numberOfArrays, userArrays);
+    std::vector<std::array<int, 7>> userNumbers;     //Vector containing a dymanic amount of 7 int element arrays
+    GenLottoNumbers(lottoType, amountOfNumbers, userNumbers);
     
-    std::vector<std::array<int, 7>> winningArray;   //Vector which should only contain one 7 int element array
-    GenLottoArrays(lottoType, 1, winningArray);
+    std::vector<std::array<int, 7>> winningNumbers;   //Vector which should only contain one 7 int element array
+    GenLottoNumbers(lottoType, 1, winningNumbers);
     std::cout << "Finished!" << std::endl << std::endl;
 
-    std::cout << "Ready to simulate " << NumToString(numberOfArrays) << " different lotto numbers on " << lottoTypeString <<std::endl;
-    std::cout << NumToString(numberOfArrays) << " " << lottoTypeString << " numbers would cost you: " << NumToString(numberOfArrays * lottoPrice) << "kr (NOK, Norwegian kroner)" << std::endl;
+    std::cout << "Ready to simulate " << NumToString(amountOfNumbers) << " different lotto numbers on " << lottoTypeString <<std::endl;
+    std::cout << NumToString(amountOfNumbers) << " " << lottoTypeString << " numbers would cost you: " << NumToString(amountOfNumbers * lottoPrice) << "kr (NOK, Norwegian kroner)" << std::endl;
     std::cout << "Press enter so start simulation. It will run until you win..." << std::endl;
     std::getchar(); 
     std::getchar(); //Press any key to contiue
-    CheckIfWin(lottoType, userArrays, winningArray);
+    CheckIfWin(lottoType, userNumbers, winningNumbers);
 }
 
-std::vector<std::array<int, 7>> GenLottoArrays (int lottoType, int numberOfArrays, std::vector<std::array<int, 7>>& vector){ 
+std::vector<std::array<int, 7>> GenLottoNumbers (int lottoType, int amountOfNumbers, std::vector<std::array<int, 7>>& vector){ 
     vector.clear();
-    for (int i=1; i<=numberOfArrays; i++){ //Loops though array generation until vector has recieved numberOfArrays arrays    
+    for (int i=1; i<=amountOfNumbers; i++){ //Loops though array generation until vector has recieved amountOfNumbers arrays    
         std::array<int, 7> array;
         int randNum;
         int arrayPlace;
@@ -157,7 +157,7 @@ std::vector<std::array<int, 7>> GenLottoArrays (int lottoType, int numberOfArray
     return vector; //This return will return an empty vector if none of the lottoTypes is selected.
 }
 
-void PrintVectorArrays(std::vector<std::array<int, 7>> vector){
+void PrintLottoNumbers(std::vector<std::array<int, 7>> vector){
     for (int i=0; i<vector.size() && i <= 9; i++){ //Loop through for every element in vector but not over 10 (9 since array start at 0) elements. If it were to print more than 10 elements in form big vecotrs, it would look messy on the output and slow the program down.
         std::array<int, 7> array = vector[i]; //Sets "array" to be the array an "i" place in the vector
         for(int x=0; x<array.size(); x++){
@@ -189,22 +189,22 @@ void CheckIfWin (int lottoType, std::vector<std::array<int, 7>> user, std::vecto
     while (std::find(user.begin(), user.end(), win[0]) == user.end()){ //Checks whether the first element in the winning vector is in the user vector, and loops until it is
         if ((w % 10000) == 0){ //Prints out every 10000th attemt so the program isn't so dull. Also so it's possbile to see some of the winning numbers which didn't match up
             std::cout << "Your lotto numbers:        Winning lotto number:" << std::endl;
-            PrintVectorArrays(user);
+            PrintLottoNumbers(user);
             std::cout << "  |";
-            PrintVectorArrays(win);
+            PrintLottoNumbers(win);
             std::cout << "  | Week/Try: " << NumToString(w) << std::endl << std::endl; //Prints week/try number and adds to new line due to formatting the conole output
         }
         w++; //Adds 1 to week/try number
         
         //Genreates new winning array since tha last didnt match any of the user arrays
-        GenLottoArrays(lottoType, 1, win);
+        GenLottoNumbers(lottoType, 1, win);
     }
 
     //If you win (winning array is in user vector)
     std::cout << "Yadigg, you won!" << std::endl;
     std::cout << "Your lotto numbers:         Winning lotto number:" << std::endl;
-    PrintVectorArrays(user);
+    PrintLottoNumbers(user);
     std::cout << "  |";
-    PrintVectorArrays(win);
+    PrintLottoNumbers(win);
     std::cout << "  | Week/Try: " << NumToString(w) << std::endl;
 }
